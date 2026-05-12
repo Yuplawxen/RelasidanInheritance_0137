28b8bc56b1f3651f180239af65d5f5edcd915709
Yuplawxen <yuftiyannaaqurrota@gmail.com>
2026-05-12 18:55:41 +0700

    Define class ibu dan membernya

--- ibu.h
+++ ibu.h
@@ -4,4 +4,21 @@ using namespace std;
 #define IBU_H
 #include <vector>
 #include "anak.h"
+
+class ibu {
+public :
+    string nama ;
+    vector<anak*> daftar_anak;
+
+    ibu (string pNama) : nama (pNama) {
+        cout << "ibu \"" << nama << "\" ada \n";
+    }
+
+    ~ibu () {
+        cout << "ibu \"" << nama << "\" tidak ada \n";
+    }
+    void tambahAnak ( anak*);
+    void cetakAnak ();
+};
+
 #endif
